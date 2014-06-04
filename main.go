776baec56a4ac121a9345148035d1d02f4efe508
Steven Rhodes <rhodes.steven@gmail.com>
2014-06-03 21:08:47 -0700

    Server hello world

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8080", r)
 }

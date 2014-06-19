a0c89d045ce915dc162fa585e83450720eae5110
Steven Rhodes <rhodes.steven@gmail.com>
2014-06-18 22:36:41 -0700

    Adding ServeLatest test

--- lib/metrics_test.go
+++ lib/metrics_test.go
@@ -1,6 +1,8 @@
 package lib_test
 
 import (
+	"database/sql"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -13,6 +15,18 @@ import (
 	"github.com/stvnrhodes/broadcaster"
 )
 
+func makeDB(t *testing.T) *lib.DB {
+	s, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	db, err := lib.NewDB(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return db
+}
+
 func getHTTP(t *testing.T, url string) string {
 	res, err := http.Get(url)
 	if err != nil {
@@ -24,6 +38,36 @@ func getHTTP(t *testing.T, url string) string {
 		t.Fatal(err)
 	}
 	return string(body)
+}
+
+func TestServeLatest(t *testing.T) {
+	db := makeDB(t)
+	bus := &msgs.BusMeasurement{BusVoltage: 0, BusCurrent: 0.5}
+	db.WriteCAN(&msgs.CANPlus{bus, msgs.GetID(bus), time.Unix(30, 0)})
+	bus = &msgs.BusMeasurement{BusVoltage: 1.5, BusCurrent: 3}
+	db.WriteCAN(&msgs.CANPlus{bus, msgs.GetID(bus), time.Unix(40, 0)})
+	v := &msgs.VelocityMeasurement{MotorVelocity: 1, VehicleVelocity: 2}
+	db.WriteCAN(&msgs.CANPlus{v, msgs.GetID(v), time.Unix(40, 0)})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/data", lib.ServeLatest(db))
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	tests := []struct{ path, want string }{
+		{"/data", `[]` + "\n"},
+		{"/data?canid=123", `[]` + "\n"},
+		{fmt.Sprintf("/data?canid=%d", msgs.GetID(bus)), `[{"CAN":{"BusVoltage":1.5,"BusCurrent":3},"canID":1026,"time":"1969-12-31T16:00:40-08:00"}]` + "\n"},
+		{fmt.Sprintf("/data?canid=%d", msgs.GetID(v)), `[{"CAN":{"MotorVelocity":1,"VehicleVelocity":2},"canID":1027,"time":"1969-12-31T16:00:40-08:00"}]` + "\n"},
+		{fmt.Sprintf("/data?canid=%d&canid=%d", msgs.GetID(bus), msgs.GetID(v)), `[{"CAN":{"BusVoltage":1.5,"BusCurrent":3},"canID":1026,"time":"1969-12-31T16:00:40-08:00"},{"CAN":{"MotorVelocity":1,"VehicleVelocity":2},"canID":1027,"time":"1969-12-31T16:00:40-08:00"}]` + "\n"},
+	}
+
+	for _, c := range tests {
+		got := getHTTP(t, ts.URL+c.path)
+		if got != c.want {
+			t.Errorf("%v: got %v, want %v", c.path, got, c.want)
+		}
+	}
 }
 
 func TestServeJSON(t *testing.T) {

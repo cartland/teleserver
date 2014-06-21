4980d724ab98d9d189dbb9628e460cb9fbe3364f
Steven Rhodes <rhodes.steven@gmail.com>
2014-06-20 19:58:09 -0700

    Four updating graphs

--- public/solar/solar.js
+++ public/solar/solar.js
@@ -3,88 +3,75 @@
     // This constant should be kept in sync with metrics.go
     var bufferedTime = 20 * 1000 // 20s * 1000 ms/s
 
-    // Create the graphs
-    var plots = {
-        "array-voltage": $.plot("#array-voltage", [], plotDefaults),
-        "array-current": $.plot("#array-current", [], plotDefaults),
-        "battery-voltage": $.plot("#battery-voltage", [], plotDefaults),
-        "temperature": $.plot("#temperature", [], plotDefaults),
-    }
+    // These metrics must match up with both the graph html ids and the json field
+    // names.
+    var metrics = ["ArrayVoltage", "ArrayCurrent", "BatteryVoltage", "Temperature"]
+    // Create maps to hold the plots, the data for each plot, and the data for each line
+    var plots = {}, dataArrays = {}, plotData = {};
 
-    var data = {}, fetched = {};
-    var dataArray = {};
-    $("#array-current").bind("plothover", tooltip());
 
-    function refreshPlot(plotname) {
-        var plot = plots[plotname];
-        debugger;
-        plot.setData(dataArray[plotname]);
-        plot.setupGrid();
-        plot.draw();
-    }
-    // Populate the graph with initial data
-    function onFetch(plotname) {
-        return function(allSeries) {
-            dataArray[plotname] = allSeries
-            refreshPlot(plotname)
-        }
-    }
+    // Refresh the plots 5 times a second.
+    window.setInterval(
+        function() {
+            for (var i = 0; i < metrics.length; i++) {
+                var plotname = metrics[i];
+                if (plots[plotname] && dataArrays[plotname])
+                    var plot = plots[plotname];
+                plot.setData(dataArrays[plotname]);
+                plot.setupGrid();
+                plot.draw();
+            }
+        }, 200);
 
-    // Fetch the initial data
-    var canids = [0x600, 0x601, 0x602, 0x603].join("&canid=")
-    $.ajax({
-        url: "/api/graphs?canid=" + canids + "&field=ArrayCurrent",
-        type: "GET",
-        dataType: "json",
-        success: onFetch("array-current"),
-    });
-    $.ajax({
-        url: "/api/graphs?canid=" + canids + "&field=ArrayVoltage",
-        type: "GET",
-        dataType: "json",
-        success: onFetch("array-voltage"),
-    });
-    $.ajax({
-        url: "/api/graphs?canid=" + canids + "&field=BatteryVoltage",
-        type: "GET",
-        dataType: "json",
-        success: onFetch("battery-voltage"),
-    });
-    $.ajax({
-        url: "/api/graphs?canid=" + canids + "&field=Temperature",
-        type: "GET",
-        dataType: "json",
-        success: onFetch("temperature"),
-    });
+    // Populate the initial values of the plots by getting historical data from
+    // ajax queries. Also fill in the dataArrays and plotData with this initial
+    // information.
+    var canids = [0x600, 0x601, 0x602, 0x603].join("&canid=");
+    for (var i = 0; i < metrics.length; i++) {
+        (function() {
+            var metric = metrics[i];
+            plots[metric] = $.plot("#" + metric, [], plotDefaults);
+            $("#" + metric).bind("plothover", tooltip());
+            $.ajax({
+                url: "/api/graphs?time=20s&canid=" + canids + "&field=" + metric,
+                type: "GET",
+                dataType: "json",
+                success: function(points) {
+                    for (var i = 0; i < points.length; i++) {
+                        plotData[points[i].label] = points[i].data
+                    }
+                    dataArrays[metric] = points
+                    // refreshPlot(metric)
+                },
+            });
+        })();
+    }
 
     $(function() {
-
-        // Update the graph with the given point
-        function update(name, point) {
-            name = "0x" + parseInt(data.canID, 10).toString(16) + " - " + name
-            if (fetched[name]) {
-                series = data[name].data;
-                series.push(point);
-                while (series.length > 1 && point[0] - series[0][0] > bufferedTime) {
-                    series.shift();
-                }
-
-                // TODO(stvn): Support multiple graphs
-                refreshPlot(plots["temperature"])
-            }
-        }
-
-        // Create a websocket connection and do live updates of the data
         var ws = new WebSocket("ws://" + window.location.host + "/ws");
         ws.onmessage = function(e) {
             var data = JSON.parse(e.data);
 
             if (data.time) {
-                data.time = (new Date(data.time)).getTime();
+                data.time = (new Date(data.time));
             }
-            if (data.canID >= 0x600 && data.canID <= 0x604) {
-                update(data.CAN.ArrayLocation, [data.time, data.CAN]);
+            if (data.CAN) {
+                // Update any graphs that match.
+                for (var i = 0; i < metrics.length; i++) {
+                    var metric = metrics[i]
+                    var name = "0x" + parseInt(data.canID, 10).toString(16) + " - " + metric
+                    if (data.CAN[metric] && plotData[name]) {
+                        var series = plotData[name];
+                        var time = data.time.getTime()
+                        series.push([time, data.CAN[metric]]);
+                        while (series.length > 1 && time - series[0][0] > bufferedTime) {
+                            series.shift();
+                        }
+                        // refreshPlot(metric)
+                    }
+                }
             }
-        }
+
+        };
     });
 })(jQuery);

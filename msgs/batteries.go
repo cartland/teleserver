19a6f4deadab8bf0a3689ec83ecdfb3f75c04532
Steven Rhodes <rhodes.steven@gmail.com>
2014-06-22 20:12:37 -0700

    Proper ID for batteries

--- msgs/batteries.go
+++ msgs/batteries.go
@@ -7,11 +7,12 @@ type BatteryHeartbeat struct {
 func (BatteryHeartbeat) New() CAN { return &BatteryHeartbeat{} }
 
 type BatteryModule struct {
-	ID       uint16
-	Voltage0 uint16 `binpack:"0-4"`
+	ID       uint16 `json:"-"`
+	Voltage0 uint16 `binpack:"0-2"`
 	Voltage1 uint16 `binpack:"2-4"`
 	Voltage2 uint16 `binpack:"4-6"`
 	Voltage3 uint16 `binpack:"6-8"`
 }
 
-func (b BatteryModule) New() CAN { return &BatteryModule{ID: b.ID} }
+func (b BatteryModule) New() CAN      { return &BatteryModule{ID: b.ID} }
+func (b BatteryModule) canID() uint16 { return b.ID }

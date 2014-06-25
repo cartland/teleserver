c114a654000b982b850ad5a61876e3d7d7317e9a
Steven Rhodes <rhodes.steven@gmail.com>
2014-06-24 19:34:28 -0700

    Adding graph of batteries

--- public/batteries/batteries.js
+++ public/batteries/batteries.js
@@ -1,4 +1,25 @@
 (function($) {
+    // Create the graph
+    var plot = $.plot("#placeholder", [], plotDefaults);
+    $("#placeholder").bind("plothover", tooltip());
+
+
+    // Populate the graph with initial data
+    function onJSONFetch(allSeries) {
+        plot.setData(allSeries);
+        plot.setupGrid();
+        plot.draw();
+    }
+
+    // Fetch the initial data
+    var canids = [0x130, 0x131, 0x132, 0x140, 0x141, 0x142].join("&canid=");
+    var fields = ["Voltage0", "Voltage1", "Voltage2", "Voltage3"].join("&field=");
+    $.ajax({
+        url: "/api/graphs?canid=" + canids + "&field=" + fields + "&time=1m",
+        type: "GET",
+        dataType: "json",
+        success: onJSONFetch
+    });
 
     $(function() {
         var ws = new WebSocket("ws://" + window.location.host + "/ws");
